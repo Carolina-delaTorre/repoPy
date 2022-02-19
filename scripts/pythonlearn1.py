ebbd5ac43d1c42a119e7654fbd7aa94aa42679b0
Carolina-delaTorre <carotorresrg@gmail.com>
2022-02-19 13:09:40 -0600

    second session

---
+++ scripts/pythonlearn1.py
@@ -0,0 +1,14 @@
+var1 = "Hola"
+var2 = "17"
+var3 = 29
+
+print(var1)
+print(var1+var2)
+print(int(var2)+var3)
+print("Hola a todos")
+
+# está función imprime un número
+def propiedad_variable_integer(valor_numerico):
+  return print("el valor número es: ", valor_numerico)
+
+propiedad_variable_integer(20)
